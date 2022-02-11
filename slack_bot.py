4be16410a7231cc410a5c8128afbc35c566a918c
Shreya <sbali@andrew.cmu.edu>
2022-02-11 17:04:53 -0500

    corrected time to be utc

--- slack_bot.py
+++ slack_bot.py
@@ -1603,7 +1603,7 @@ def slack_oauth():
 
 
 # TODO change this to hour for final
-@cron.task("cron", hour="8")
+@cron.task("cron", hour="14")
 def post_stories():
     remove_past_nooks(slack_app, db, nooks_alloc)
     current_nooks = list(db.nooks.find({"status": "show"}))
@@ -1622,7 +1622,7 @@ def post_stories():
             )
 
 
-@cron.task("cron", hour="16")
+@cron.task("cron", hour="23")
 def update_stories():
     suggested_nooks = update_nook_suggestions(slack_app, db)
     nooks_home.update(suggested_nooks=suggested_nooks)
