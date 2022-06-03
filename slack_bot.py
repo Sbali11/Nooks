da88a98b036a27e02def8b73ac0a7d3203f28228
Shreya <sbali@andrew.cmu.edu>
2022-06-02 21:29:29 -0400

    removed trial cron job

--- slack_bot.py
+++ slack_bot.py
@@ -2520,14 +2520,14 @@ def get_team_rows_timezone(time, skip_weekends=True):
 
     return team_ids
 
-
+'''
 @cron.task("cron", second="0")
 def trial():
     remove_stories_periodic(["T03CY3C0LRF"])
     all_team_rows_no_weekend = ["T03CY3C0LRF"]
     post_stories_periodic(all_team_rows_no_weekend)
     update_stories_periodic(all_team_rows_no_weekend)
-
+'''
 @cron.task("cron", minute="0")
 def post_stories_0():
     remove_stories_periodic(get_team_rows_timezone("12:00", skip_weekends=False))
