4a70bdc94cf0dc8c8bbed05778f8df4f69bf8998
Shreya <sbali@andrew.cmu.edu>
2022-06-06 17:15:34 -0400

    time

--- slack_bot.py
+++ slack_bot.py
@@ -2539,10 +2539,10 @@ def post_stories_0():
     update_stories_periodic(all_team_rows_no_weekend)
 
 
-@cron.task("cron", minute="57")
+@cron.task("cron", minute="30")
 def post_stories_30():
-    remove_stories_periodic(get_team_rows_timezone("13:57", skip_weekends=False))
-    all_team_rows_no_weekend = get_team_rows_timezone("13:57")
+    remove_stories_periodic(get_team_rows_timezone("12:00", skip_weekends=False))
+    all_team_rows_no_weekend = get_team_rows_timezone("12:00")
     post_stories_periodic(all_team_rows_no_weekend)
     update_stories_periodic(all_team_rows_no_weekend)
 
