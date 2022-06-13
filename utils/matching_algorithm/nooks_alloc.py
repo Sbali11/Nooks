d544a2ff4aa5a6e070bc571dffd2faffa9507fd5
Shreya <sbali@andrew.cmu.edu>
2022-06-13 19:04:31 -0400

    partitioning in try-except block; code to simulate partitioning

--- utils/matching_algorithm/nooks_alloc.py
+++ utils/matching_algorithm/nooks_alloc.py
@@ -5,6 +5,7 @@ from gc import collect
 import logging
 from multiprocessing import context
 from re import L
+from turtle import right
 import numpy as np
 import random
 from utils.constants import *
@@ -21,7 +22,6 @@ class NooksAllocation:
         self.all_members_ids = collections.defaultdict(set)
         for team_row in list(self.db.tokens_2.find()):
             self._create_members(team_row["team_id"])
-            
 
     def _create_partitions(self, team_id):
         allocated = set([])
@@ -68,7 +68,7 @@ class NooksAllocation:
             g = Graph(blacklist_edges, bnodes, len(bnodes))
             while not kcoloring_res:
                 print(num_partitions, kcoloring_res)
-                
+
                 kcoloring_res = kColoring(
                     g, [0] * len(bnodes), num_partitions, 0, len(bnodes)
                 )
@@ -78,8 +78,8 @@ class NooksAllocation:
         print(kcoloring_res)
         partitions = [set([]) for i in range(num_partitions)]
         for member_idx in range(len(kcoloring_res)):
-            partitions[kcoloring_res[member_idx]-1].add(bnodes[member_idx])
-            partitions_alloc[bnodes[member_idx]] = kcoloring_res[member_idx]-1
+            partitions[kcoloring_res[member_idx] - 1].add(bnodes[member_idx])
+            partitions_alloc[bnodes[member_idx]] = kcoloring_res[member_idx] - 1
 
         current_part = 0
         for member in oldmembers_list:
@@ -129,14 +129,18 @@ class NooksAllocation:
     # TODO change to team specific?
     def _create_members(self, team_id):
         # np.zeros((len(all_members), len(all_members)))
-        
+
         all_members = list(self.db.member_vectors.find({"team_id": team_id}))
         self.member_vectors[team_id]["newcomers"] = []
         self.member_vectors[team_id]["oldmembers"] = []
         self.all_members_ids[team_id] = set([])
         for member in all_members:
             self.all_members_ids[team_id].add(member["user_id"])
-            if member["Role"] not in  ["Old Company Employee(More than 6 months)", "Professor","PhD Student"]:
+            if member["Role"] not in [
+                "Old Company Employee(More than 6 months)",
+                "Professor",
+                "PhD Student",
+            ]:
                 print(member["Role"])
                 self.member_vectors[team_id]["newcomers"].append(member)
             else:
@@ -156,71 +160,111 @@ class NooksAllocation:
             if nook["creator"] in self.all_members_ids[team_id]:
                 right_swiped.append(nook["creator"])
                 right_swiped_set = set(right_swiped)
-                
-                if len(right_swiped_set) < 2 :
+
+                if len(right_swiped_set) < 2:
                     mems = []
-                elif len(right_swiped_set) < 3 and not(nook["allow_two_members"]):
+                elif len(right_swiped_set) < 3 and not (nook["allow_two_members"]):
                     mems = []
-                else :
+                else:
                     mems = right_swiped_set
-                
+
                 nooks_allocs[nook["_id"]] = ",".join(list(set(mems)))
 
             else:
-                nook_part_id = {}
-
-                partitioned_nooks_members = [set([]) for i in range(len(partitions))]
-                for p in range(len(partitions)):
-                    for member in partitions[p]:
-                        if member in right_swiped_set:
-                            partitioned_nooks_members[p].add(member)
-                partitioned_nooks_members.sort(key=len)
-                
-                start_merge_index = 0
-                last_merge_index = len(partitioned_nooks_members)-1
-                print(partitioned_nooks_members)
-
-                for i in range(len(partitioned_nooks_members)):
-                    if len(partitioned_nooks_members[i]) == 0:
-                        start_merge_index = i + 1
-                    if len(partitioned_nooks_members[i]) >= MIN_NUM_MEMS:
-                        last_merge_index = i - 1
-                i = start_merge_index
-                j = len(partitioned_nooks_members) - 1
-                merged = {i: i for i in range(len(partitions))}
-                merge_idx = len(partitions)
-                while i <= last_merge_index:
-                    total_num = partitioned_nooks_members[i]
-                    i += 1
-                    while total_num < MIN_NUM_MEMS and i < len(partitioned_nooks_members):
-                        total_num += partitioned_nooks_members[i]
-                        merged[i] = merge_idx
-                        i += 1
-                    merge_idx += 1
-
-                nook_mems = [set([]) for _ in range(merge_idx)]
-                for i in range(len(partitions)):
-                    nook_mems[merged[i]] = nook_mems[merged[i]].union(partitioned_nooks_members[i])
-
-                for merged_part_idx in range(len(nook_mems)):
-
-                    if nook_mems[merged_part_idx]:
-                        nook_info = {key: nook[key] for key in nook if not(key=="_id")}
-                        nook_info["members"] = ",".join(
-                            list(nook_mems[merged_part_idx])
+                try:
+                    nook_part_id = {}
+                    partitioned_nooks_members = [set([]) for i in range(len(partitions))]
+                    random.shuffle(right_swiped)
+                    right_swiped_set = set(right_swiped)
+                    i = 0
+                    partitions_has_changed = False
+                    for member in set(right_swiped_set):
+                        if member not in partitions_alloc:
+                            partitions_has_changed = True
+                            partitions[i].add(member)
+                            partitions_alloc[member] = i
+                            i = (i + 1) % len(partitions)
+                    if partitions_has_changed:
+                        my_date = datetime.date.today()  # if date is 01/01/2018
+                        year, week_num, day_of_week = my_date.isocalendar()
+                        self.db.temporal_partitions.update_one(
+                        {"team_id": team_id, "week_num": week_num, "year": year},
+                        {
+                            "team_id": team_id,
+                            "week_num": week_num,
+                            "year": year,
+                            "partitions_alloc": partitions_alloc,
+                            "partitions": [list(p) for p in partitions],
+                        },
                         )
-                        nook_part_i = self.db.nooks.insert_one(nook_info)
-                        nooks_allocs[nook_part_i.inserted_id] = nook_info["members"]
-                        nook_part_id[merged_part_idx] = nook_part_i.inserted_id
 
-                nooks_allocs[nook["_id"]] = ""
+                    for p in range(len(partitions)):
+                        for member in partitions[p]:
+                            if member in right_swiped_set:
+                                partitioned_nooks_members[p].add(member)
+                    partitioned_nooks_members.sort(key=len)
 
-                self.db.nooks.update_one(
+                    start_merge_index = 0
+                    last_merge_index = len(partitioned_nooks_members) - 1
+                    print(partitioned_nooks_members)
+
+                    for i in range(len(partitioned_nooks_members)):
+                        if len(partitioned_nooks_members[i]) == 0:
+                            start_merge_index = i + 1
+                        if len(partitioned_nooks_members[i]) >= MIN_NUM_MEMS:
+                            last_merge_index = i - 1
+                    i = start_merge_index
+                    j = len(partitioned_nooks_members) - 1
+                    merged = {i: i for i in range(len(partitions))}
+                    merge_idx = len(partitions)
+                    while i <= last_merge_index:
+                        total_num = len(partitioned_nooks_members[i])
+                        i += 1
+                        while total_num < MIN_NUM_MEMS and i < len(
+                        partitioned_nooks_members
+                        ):
+                            total_num += len(partitioned_nooks_members[i])
+                            merged[i] = merge_idx
+                            i += 1
+                        merge_idx += 1
+
+                    nook_mems = [set([]) for _ in range(merge_idx)]
+                    for i in range(len(partitions)):
+                        nook_mems[merged[i]] = nook_mems[merged[i]].union(
+                            partitioned_nooks_members[i]
+                        )
+                    nook_mems.sort(key=len)
+                    n_nook_mems = []
+                    for n in nook_mems:
+                        if n:
+                            n_nook_mems.append(n)
+                    
+                    if len(n_nook_mems) > 1 and len(n_nook_mems[0])< MIN_NUM_MEMS:
+                        n_nook_mems[1] = n_nook_mems[1].union({k for k in n_nook_mems[0]})
+                        n_nook_mems[0] = set([])
+
+                    for merged_part_idx in range(len(n_nook_mems)):
+
+                        if n_nook_mems[merged_part_idx]:
+                            nook_info = {
+                            key: nook[key] for key in nook if not (key == "_id")
+                            }
+                            nook_info["members"] = ",".join(
+                                list(n_nook_mems[merged_part_idx])
+                            )
+                            nook_part_i = self.db.nooks.insert_one(nook_info)
+                            nooks_allocs[nook_part_i.inserted_id] = nook_info["members"]
+                            nook_part_id[merged_part_idx] = nook_part_i.inserted_id
+
+                    nooks_allocs[nook["_id"]] = ""
+
+                    self.db.nooks.update_one(
                     {"_id": nook["_id"]}, {"$set": {"status": "duplicated"}}
-                )
+                    )
+                except Exception as e:
+                    print(e)
 
-
-        return nooks_allocs, {} 
+        return nooks_allocs, {}
 
     def _create_alloc_suggestions(
         self,
@@ -237,7 +281,7 @@ class NooksAllocation:
             if merged_partitions[partitions_alloc[member]] in nook_part_id:
 
                 suggested_allocs_list[
-                nook_part_id[merged_partitions[partitions_alloc[member]]]
+                    nook_part_id[merged_partitions[partitions_alloc[member]]]
                 ].append(member)
 
         for nook_id in suggested_allocs_list:
